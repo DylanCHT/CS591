223b8af88976f41f9e1347dca40b0eb51f97a834
DylanCHT <43015876+DylanCHT@users.noreply.github.com>
2019-05-26 23:49:35 -0400

    Add files via upload

--- PS1/PS1.P1.js
+++ PS1/PS1.P1.js
@@ -1,19 +1,2 @@
-const getOperation = str => {
-	switch (str[1]) {
-	 	case '+':
-	 		return (left, right) => str[0] + str[2];
-	 	case '-':
-	 		return (left, right) => str[0] - str[2];
-	 	case '%':
-	 		return (left, right) => str[0] % str[2]:
-	 	case '/'
-	 		return (left, right) => str[0] % str[2]:
-    }
-}
-
-const expression = '8%3';
-let operator = evaluate(expression);
-console.log(`${expression} = ${operator(expression)}`) 
-//let mathFunction = getOperation('+');
-//console.log(mathFunction(30,12))
-
+const sortAlph = str => [...str].sort().join('').replace(/\d+/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+console.log(`New order: ${sortAlph("supercalifragilisticexpialidocious")}`);
