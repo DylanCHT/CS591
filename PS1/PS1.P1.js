580298aeb46e044526eb8b0c12eb9717495d0c71
Dylan  Tao <dylantao@dhcp-wifi-8021x-155-41-53-228.bu.edu>
2019-06-05 10:37:40 -0400

    rem PS12

--- PS1/PS1.P1.js
+++
@@ -1,2 +0,0 @@
-const sortAlph = str => [...str].sort().join('').replace(/\d+/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-console.log(`New order: ${sortAlph("supercalifragilisticexpialidocious")}`);
--- PS2/PS2.P3.js
+++
@@ -1,5 +0,0 @@
-const cube = x => x**3;
-const func = num => num.map(x => cube(x));
-
-let lst = [1,2,3,4,5,6,7];
-console.log(`Cube value of list ${lst} : ${func(lst)}`);
